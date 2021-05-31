5b7b6298dc00b272783dbb5aeb77c55d086e0f9b
Tjakka5 <justinvanderleij@gmail.com>
2021-05-31 16:39:54 +0200

    Send joystick

--- Project/Project.ino
+++ Project/Project.ino
@@ -7,13 +7,15 @@
 
 RF24 radio(14, 15);
 uint8_t address[][6] = {"1Node", "2Node"};
-bool radioNumber     = 1;
+bool radioNumber     = 0;
 bool role            = false;
-uint16_t payload[4]        = { };
+byte payload;
 
 int incomingByte = 0;
 
 byte incomingBytes[10];
+
+byte lastJoystickValue = 0;
 
 typedef void (*commandBody)(byte[10]);
 
@@ -26,22 +28,25 @@ typedef struct
 command commandLookup[256];
 
 DynamixelInterface dInterface(Serial2, 2);
-DynamixelMotor motors[4] = {
+DynamixelMotor servos[4] = {
     DynamixelMotor(dInterface, 0),
     DynamixelMotor(dInterface, 1),
     DynamixelMotor(dInterface, 2),
     DynamixelMotor(dInterface, 3),
 };
 
+int motors[4] = {
+    23, 22, 21, 20};
+
 unsigned long lastUpdate;
 
-void handleSetServoTargetPosition(byte incomingBytes[10])
+void handleSetServoTargetDegree(byte incomingBytes[10])
 {
     Serial.println("Servo target position");
     uint8_t id        = incomingBytes[1];
     uint16_t position = (incomingBytes[3] << 8) + incomingBytes[2];
 
-    motors[id].goalPosition(position);
+    servos[id].goalPositionDegree(position);
 }
 
 void handleSetServoLight(byte incomingBytes[10])
@@ -50,7 +55,7 @@ void handleSetServoLight(byte incomingBytes[10])
     uint8_t id = incomingBytes[1];
     bool state = incomingBytes[2];
 
-    motors[id].led(state);
+    servos[id].led(state);
 }
 
 void handleSetServoSpeed(byte incomingBytes[10])
@@ -59,27 +64,32 @@ void handleSetServoSpeed(byte incomingBytes[10])
     uint8_t id     = incomingBytes[1];
     uint16_t speed = (incomingBytes[3] << 8) + incomingBytes[2];
 
-    motors[id].speed(speed);
+    servos[id].speed(speed);
+}
+
+void handleSetMotorPwm(byte incomingBytes[10])
+{
+    Serial.println("Motor pwm");
+    uint8_t id     = incomingBytes[1];
+    uint16_t speed = incomingBytes[2];
+
+    Serial.println(speed);
+    analogWrite(motors[id], speed);
 }
 
 void setup()
 {
     Serial.begin(115200);
-    while (!Serial) {}
+    // while (!Serial) {}
     if (!radio.begin())
     {
         Serial.println(F("radio hardware is not responding!!"));
         while (1) {}
     }
     Serial.println(F("Which radio is this? Enter '0' or '1'. Defaults to '0'"));
-    while (!Serial.available()) {}
-    char input  = Serial.parseInt();
-    radioNumber = input == 1;
-
-    radio.setPALevel(RF24_PA_LOW);
-    radio.setPayloadSize(sizeof(payload));
-    radio.openWritingPipe(address[radioNumber]);
-    radio.openReadingPipe(1, address[!radioNumber]);
+    //while (!Serial.available()) {}
+    //char input  = Serial.parseInt();
+    radioNumber = false;
 
     Serial.print(F("radioNumber = "));
     Serial.println((int)radioNumber);
@@ -88,8 +98,8 @@ void setup()
     Serial.print(F("Payload size="));
     Serial.println(radio.getPayloadSize());
 
-    radio.setPALevel(RF24_PA_LOW);
-    radio.setPayloadSize(8);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setPayloadSize(1);
     radio.openWritingPipe(address[radioNumber]);
     radio.openReadingPipe(1, address[!radioNumber]);
 
@@ -105,7 +115,7 @@ void setup()
     Serial3.begin(9600);
 
     commandLookup[0x01].requiredBytes = 4;
-    commandLookup[0x01].body          = &handleSetServoTargetPosition;
+    commandLookup[0x01].body          = &handleSetServoTargetDegree;
 
     commandLookup[0x02].requiredBytes = 3;
     commandLookup[0x02].body          = &handleSetServoLight;
@@ -113,16 +123,23 @@ void setup()
     commandLookup[0x03].requiredBytes = 4;
     commandLookup[0x03].body          = &handleSetServoSpeed;
 
+    commandLookup[0x04].requiredBytes = 3;
+    commandLookup[0x04].body          = &handleSetMotorPwm;
+
     dInterface.begin(115200, 50);
 
     for (byte i = 0; i < 4; i++)
     {
-        motors[i].init();
-        motors[i].statusReturnLevel(2);
-        motors[i].jointMode();
-        motors[i].enableTorque();
-        motors[i].speed(1023);
-        motors[i].torqueLimit(1023);
+        servos[i].init();
+        servos[i].statusReturnLevel(2);
+        servos[i].jointMode();
+        servos[i].enableTorque();
+        servos[i].speed(1023);
+        servos[i].torqueLimit(1023);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        analogWrite(motors[i], LOW);
     }
 }
 
@@ -143,6 +160,7 @@ void handleCommand()
 
         if (i == 0)
         {
+            Serial.println("New command");
             command = commandLookup[incomingByte];
         }
         else
@@ -150,10 +168,12 @@ void handleCommand()
             incomingBytes[i] = incomingByte;
         }
 
+        Serial.println(incomingByte);
         i++;
 
         if (i == command.requiredBytes)
         {
+            Serial.println("Command done");
             break;
         }
     }
@@ -168,44 +188,55 @@ void loop()
         handleCommand();
     }
 
-    if (role) {
+    if (role)
+    {
         // TODO: Send packets
-    } else {
+    }
+    else
+    {
         uint8_t pipe;
 
-        if (radio.available(&pipe)) {
+        if (radio.available(&pipe))
+        {
             uint8_t bytes = radio.getPayloadSize();
 
             radio.read(&payload, bytes);
 
-            Serial.print("Received ");
-            Serial.println(payload[0]);
-            Serial.println(payload[1]);
-            Serial.println(payload[2]);
-            Serial.println(payload[3]);
+            if ((payload & 0b01000000) == 0b01000000)
+            {
+                lastJoystickValue = payload & 0b00111111;
+            }
 
-            motors[0].goalPosition(map(payload[0], 0, 1023, 1023 - 120, 1023 - 560));
-            motors[1].goalPosition(map(payload[1], 0, 1023, 120, 560));
-            motors[2].goalPosition(map(payload[2], 0, 1023, 120, 560));
-            motors[3].goalPosition(map(payload[3], 0, 1023, 1023 - 120, 1023 - 560));
-            // motors[1].goalPosition(payload[1]);
-            // motors[2].goalPosition(payload[2]);
-            // motors[3].goalPosition(payload[3]);
+            // servos[0].goalPosition(map(payload[0], 0, 1023, 1023 - 120, 1023 - 560));
+            // servos[1].goalPosition(map(payload[1], 0, 1023, 120, 560));
+            // servos[2].goalPosition(map(payload[2], 0, 1023, 120, 560));
+            // servos[3].goalPosition(map(payload[3], 0, 1023, 1023 - 120, 1023 - 560));
+            // servos[1].goalPosition(payload[1]);
+            // servos[2].goalPosition(payload[2]);
+            // servos[3].goalPosition(payload[3]);
         }
     }
 
     if (millis() - lastUpdate > 100)
     {
+        // Motors
         for (uint8_t i = 0; i < 4; i++)
         {
             uint16_t pos;
-            DynamixelStatus status = motors[0].currentPosition(pos);
+            DynamixelStatus status = servos[0].currentPosition(pos);
 
             uint8_t* _pos = (uint8_t*)&pos; // The fuck
 
             uint8_t data[] = {0x00, i, _pos[0], _pos[1]};
 
             Serial3.write(data, 4);
+        }
+
+        // Joystick
+        {
+            uint8_t data[] = {0x01, lastJoystickValue};
+
+            Serial3.write(data, 2);
         }
         //Serial3.flush();
 
